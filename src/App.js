784afa76fd87076911135778de33d711dd850553
GI_WON <rldnjsqhdl@naver.com>
2023-12-11 14:06:21 +0900

    giwonCommit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import MainContents from './pages/mainpage/MainContents'
+import CompanyDetails from './pages/company/CompanyDetails';
+import Recommendation from './pages/recommendation/Recommendation';
 
 function App() { 
 
@@ -13,7 +15,14 @@ function App() {
         <Route path='/' element={<Layout/>}>
           {/* 메인 */}
           <Route index element={<MainContents/>}/>
+          
+          <Route path='/companyDetails' element={<CompanyDetails />} />
+          <Route path='/recommendation' element={<Recommendation />} />
+          
         </Route>
+
+
+        
       </Routes>
     </BrowserRouter>
   );
