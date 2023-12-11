5581b1886731fbe15544948398d019f41b2f5850
kang chankyu <gangchanggyu9@gmail.com>
2023-12-11 16:17:35 +0900

    기본틀

---
+++ src/main/java/com/jsg/ahispringboot/AhiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jsg.ahispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhiSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsg/ahispringboot/AhiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsg.ahispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
