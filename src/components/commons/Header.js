e524f2cc0ec1c1a9faa4b7dffdcb4c69b36446d9
{daheeh} <odaheei@gmail.com>
2023-12-11 14:30:01 +0900

    push main

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <>
             <div className={styles.headerWrapper}>
                 <div className={styles.headerMenu}>
-                    {/* 로그인 / 로그아웃 */}
+                    {/* 로그인 or 로그아웃 */}
                     {/* 세션에 사용자 정보 있는 경우 '로그아웃', 없는 경우 '로그인'으로 표시 -> 일단 로그인으로 연결할게요. */}
                     <div className={styles.asideMenu}>
                         <div className={styles.asideMenuList}>
