5669d3e2bb1418d828fe964ac5de573d706d789e
Heonamkyu93 <heonamkyu930527@gmail.com>
2023-12-12 17:09:57 +0900

    2023/12/12 회원가입 중복체크 비동기통신성공

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Layout from "./layouts/layout";
 import MainContents from './pages/mainpage/MainContents'
 import CompanyDetails from './pages/company/CompanyDetails';
 import Recommendation from './pages/recommendation/Recommendation';
-
-function App() { 
+import LoginForm from './pages/login/LoginForm';
+import JoinForm from './pages/join/JoinForm';
+import FindForm from './pages/find/FindForm';
+import MemberUpdate from './pages/memberUpdate/MemberUpdate';
+import Withdrawal from './pages/withdrawal/Withdrawal';
+import Mypage from './pages/mypage/Mypage';
+function App() {
 
 
 
@@ -15,14 +20,19 @@ function App() {
         <Route path='/' element={<Layout/>}>
           {/* 메인 */}
           <Route index element={<MainContents/>}/>
-          
+
           <Route path='/companyDetails' element={<CompanyDetails />} />
           <Route path='/recommendation' element={<Recommendation />} />
-          
+          <Route path="/loginForm" element={<LoginForm />} />
+          <Route path="/joinForm" element={<JoinForm />} />
+          <Route path="/findForm" element={<FindForm />} />
+          <Route path="/member_update" element={<MemberUpdate />} />
+          <Route path="/withdrawal" element={<Withdrawal />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Route>
 
 
-        
+
       </Routes>
     </BrowserRouter>
   );
--- src/pages/join/JoinForm.js
+++ src/pages/join/JoinForm.js
@@ -1,18 +1,68 @@
 import React from 'react';
 import styles from './JoinForm.module.css';
-
+import { useState } from "react";
+import axios from 'axios';
 const JoinForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    name:"",
+    phoneNumber:"",
+  });
+  const [isEmailChecked, setIsEmailChecked] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isEmailChecked) {
+      alert('이메일 중복 확인을 해주세요.');
+      return;
+    }
+    if(formData.password!==formData.confirmPassword) {
+      alert('비밀번호와 비밀번호확인은 같은 값이어야 합니다.');
+      return;
+    }else if (formData.phoneNumber.length < 10) {
+      alert("전화번호는 최소 10자리 이상 이어야 합니다.");
+      return;
+    } else if (formData.password.length <10 || formData.confirmPassword.length <10){
+      alert("비밀번호와 비밀번호 확인은 11자리 이상 이어야 합니다.");
+      return;
+    }
+
+  };
+  const handleEmailCheck = () => {
+    axios.get(`http://localhost:8001/api/email_duplication_check?email=${formData.email}`)
+        .then(response => {
+          // 응답 처리
+          console.log(response.data);
+        })
+        .catch(error => {
+          // 오류 처리
+          console.error('Error fetching data: ', error);
+        })
+        .finally(() => {
+          // 항상 실행되는 로직 (옵션)
+        });
+
+    setIsEmailChecked(true);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
       <div className={styles.formContainer}>
             <h2>회원가입</h2>
+
         <div className={styles.inputContainer}>
-          <label htmlFor="email" className={styles.labelEmail}>이메일</label>
+          <label htmlFor="email" >이메일</label>
           <div className={styles.inputWithButton}>
           <div className={styles.inputOnly}>
-            <input type="email" id="email" />
+            <input type="email" id="email" name="email" value={formData.email} required onChange={handleChange}/>
             </div>
-            <button className={styles.joinBtn}>중복확인</button>
+            <button className={styles.joinBtn} onClick={handleEmailCheck} type='button'>중복확인</button>
           </div>
           <span></span>
         </div>
@@ -20,7 +70,7 @@ const JoinForm = () => {
         <div className={styles.inputContainer}>
           <label htmlFor="password">비밀번호</label>
           <div className={styles.inputOnly}>
-            <input type="password" id="password" />
+            <input type="password" id="password" name="password" value={formData.password} required onChange={handleChange} placeholder='비밀번호는 11자리 이상 이어야 합니다.'/>
           </div>
           <span></span>
         </div>
@@ -28,7 +78,7 @@ const JoinForm = () => {
         <div className={styles.inputContainer}>
           <label htmlFor="passwordConfirm">비밀번호 확인</label>
           <div className={styles.inputOnly}>
-            <input type="password" id="passwordConfirm" />
+            <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} required onChange={handleChange} placeholder='비밀번호와 같은 값을 입력해주세요.'/>
           </div>
           <span></span>
         </div>
@@ -36,7 +86,7 @@ const JoinForm = () => {
         <div className={styles.inputContainer}>
           <label htmlFor="name">이름</label>
           <div className={styles.inputOnly}>
-            <input type="text" id="name" />
+            <input type="text" id="name" name="name" value={formData.name} required onChange={handleChange}/>
           </div>
           <span></span>
         </div>
@@ -52,7 +102,7 @@ const JoinForm = () => {
         <div className={styles.inputContainer}>
           <label htmlFor="phoneNumber">전화번호</label>
           <div className={styles.inputOnly}>
-            <input type="number" id="phoneNumber" minLength={10} maxLength={11} />
+            <input type="number" id="phoneNumber"  name='phoneNumber' value={formData.phoneNumber} required onChange={handleChange} placeholder='전화번호는 10자리이상이어야 합니다.'/>
           </div>
           <span></span>
         </div>
@@ -102,10 +152,11 @@ const JoinForm = () => {
           </div>
         </div> */}
         <div className={styles.inputContainer}>
-            <button className={styles.joinBtn}>회원가입</button>
+            <button className={styles.joinBtn} type="submit">회원가입</button>
           <span></span>
         </div>
       </div>
+      </form>
     </div>
   );
 };
