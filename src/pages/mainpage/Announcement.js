87314e9ec0d3a2987864c9951a2eebd1f827eb18
{daheeh} <odaheei@gmail.com>
2023-12-12 15:30:41 +0900

    캐러셀, 버튼 수정 완료

--- src/pages/mainpage/Announcement.js
+++ src/pages/mainpage/Announcement.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Announcement.module.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; 
 
 
 
@@ -9,7 +10,7 @@ const Announcement = () => {
     const numToShow = 4; // 한 번에 보여줄 공고 수
 
     const nextAnnouncements = () => {
-        if (startIndex + numToShow < totalAnnouncements) {
+        if (startIndex + numToShow < 12) {
             setStartIndex(startIndex + numToShow);
         }
     };
@@ -19,21 +20,6 @@ const Announcement = () => {
             setStartIndex(startIndex - numToShow);
         }
     };
-
-    // 전체 공고 데이터
-    const announcements = [
-        { id: 0, title: "DMS 개발 및 운영 담당" },
-        { id: 1, title: "엔씨소프트 부문별 수시 채용" },
-        { id: 2, title: "웹(JAVA/JSP)개발/시스템 운영" },
-        { id: 3, title: "클라우드 엡 애플리케이션 개발자" },
-        { id: 4, title: "파이썬 개발자" },
-        { id: 5, title: "파이썬 개발자" },
-        { id: 6, title: "파이썬 개발자" },
-        { id: 7, title: "파이썬 개발자" },
-        { id: 8, title: "파이썬 개발자" },
-    ];
-
-    const totalAnnouncements = announcements.length;
 
 
     return (
@@ -46,8 +32,8 @@ const Announcement = () => {
                             <a href="/announcement" className={styles.seeAllAnm}>
                                 전체공고
                             </a>
-                            <button className={styles.beforeBtn} onClick={prevAnnouncements}>이전</button>
-                            <button className={styles.afterBtn} onClick={nextAnnouncements}>다음</button>
+                            <button className={styles.beforeBtn} onClick={prevAnnouncements} disabled={startIndex === 0}><FaChevronLeft style={{ color: 'gray' }}/></button>
+                            <button className={styles.afterBtn} onClick={nextAnnouncements} disabled={startIndex + numToShow >= 12}><FaChevronRight style={{ color: 'gray' }}/></button>
                         </div>
                     </aside>
                 {/* 채용 공고 리스트(데이터 인덱스로 보여주기) */}
@@ -124,7 +110,7 @@ const Announcement = () => {
                                     </div>
                                 </div>
                             </div>
-                            {/* 공고 5 */}
+                            {/* 공고 6 */}
                             <div data-index="5" className={styles.slick}>
                                 <div className={styles.jobContainer}>
                                     <div data-cy="jobCard">
@@ -137,7 +123,7 @@ const Announcement = () => {
                                     </div>
                                 </div>
                             </div>
-                            {/* 공고 5 */}
+                            {/* 공고 7 */}
                             <div data-index="6" className={styles.slick}>
                                 <div className={styles.jobContainer}>
                                     <div data-cy="jobCard">
@@ -145,6 +131,71 @@ const Announcement = () => {
                                         <div className={styles.jobThumbnail}></div>
                                         <div>
                                             <p>파이썬 개발자</p>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 공고 8 */}
+                            <div data-index="7" className={styles.slick}>
+                                <div className={styles.jobContainer}>
+                                    <div data-cy="jobCard">
+                                        <a href="/announcement/3">
+                                        <div className={styles.jobThumbnail}></div>
+                                        <div>
+                                            <p>윈도우 어플리케이션 개발 및 유지보수</p>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 공고 9 */}
+                            <div data-index="8" className={styles.slick}>
+                                <div className={styles.jobContainer}>
+                                    <div data-cy="jobCard">
+                                        <a href="/announcement/3">
+                                        <div className={styles.jobThumbnail}></div>
+                                        <div>
+                                            <p>React 풀스택 개발</p>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 공고 10 */}
+                            <div data-index="9" className={styles.slick}>
+                                <div className={styles.jobContainer}>
+                                    <div data-cy="jobCard">
+                                        <a href="/announcement/3">
+                                        <div className={styles.jobThumbnail}></div>
+                                        <div>
+                                            <p>자연어처리(멀티모달) 엔지니어</p>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 공고 11 */}
+                            <div data-index="10" className={styles.slick}>
+                                <div className={styles.jobContainer}>
+                                    <div data-cy="jobCard">
+                                        <a href="/announcement/3">
+                                        <div className={styles.jobThumbnail}></div>
+                                        <div>
+                                            <p>DBA 채용</p>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* 공고 12 */}
+                            <div data-index="11" className={styles.slick}>
+                                <div className={styles.jobContainer}>
+                                    <div data-cy="jobCard">
+                                        <a href="/announcement/3">
+                                        <div className={styles.jobThumbnail}></div>
+                                        <div>
+                                            <p>Senior FrontEnd Engineer</p>
                                         </div>
                                         </a>
                                     </div>
