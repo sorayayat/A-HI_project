3077c87d06d73d50ca2890ae27c3981c02a76385
kang chankyu <gangchanggyu9@gmail.com>
2023-12-14 20:56:35 +0900

    react 에서 받은 유저 정보로 gpt한테 답변 받고 다시 반환하는것까지는 구현 완료

--- inspection/inspection.py
+++ inspection/inspection.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter , Request
+from typing import List
+from pydantic import BaseModel
+from inspection import inspectionPrompt
 
 ITrouter = APIRouter(prefix="/inspection")
 
+class Ask(BaseModel):
+    introductionTitle : List[str]
+    keyword : List[str]
+    content : List[str]
+    title : str
+
+
 @ITrouter.post("/aks")
-async def ask(request : Request):
-    print(request)
-    return {"status" : 200}
+async def ask(ask : Ask):
+    result = inspectionPrompt.create_prediction_prompt(ask)
+    print(f"result : {result}")
+    return {"status" : 200 , "result" : result}
