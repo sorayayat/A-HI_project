6d587977dd97c971bdfec3ad6e68c3e3097197ad
kang chankyu <gangchanggyu9@gmail.com>
2023-12-16 16:37:37 +0900

    현재 entity 작성 중

--- src/main/java/com/jsg/ahispringboot/inspection/controller/InspectionController.java
+++ src/main/java/com/jsg/ahispringboot/inspection/controller/InspectionController.java
@@ -1,4 +1,19 @@
 package com.jsg.ahispringboot.inspection.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RestController
+@RequestMapping("/inspection")
 public class InspectionController {
+
+    @GetMapping("/getResume")
+    public String selectResume() {
+
+        return "Hello World";
+    }
+
 }
