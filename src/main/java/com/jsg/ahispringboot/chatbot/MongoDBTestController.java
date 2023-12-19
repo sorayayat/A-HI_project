2df116d731f6ee89caa355ed42d5f3948c088a7a
{daheeh} <odaheei@gmail.com>
2023-12-19 18:28:56 +0900

    mongoDB selcet, insert 동작 확인

--- src/main/java/com/jsg/ahispringboot/chatbot/MongoDBTestController.java
+++ src/main/java/com/jsg/ahispringboot/chatbot/MongoDBTestController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -19,11 +21,16 @@ public class MongoDBTestController {
         return mongoDBTestService.selectUser(name);
     }
 
-    @GetMapping(value = "/save")
-    public String saveUserData(@RequestParam String name, @RequestParam String contents) {
+    @PostMapping(value = "/save")
+    public String saveUserData(@RequestBody MongoDBTestModel data) {
+        String name = data.getName();
+        String contents = data.getContents();
+
         log.info("[Controller][Recv] name : {}, contents : {}", name, contents);
         mongoDBTestService.saveUser(name, contents);
 
         return mongoDBTestService.selectUser(name);
     }
+
+
 }
