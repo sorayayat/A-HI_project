085c1ca4f8e63bcbbceb56de8774c90b220f5ec7
{daheeh} <odaheei@gmail.com>
2023-12-21 13:29:08 +0900

    #6 리액트 서버와 연결, api요청 응답 성공

--- chatting/chattingList.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-CLrouter = APIRouter(prefix="/chatting")
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from chatting.chattingList import CLrouter
+from chatbot.chatbot import CBrouter
+
 
 app = FastAPI()
 
-app.include_router(CLrouter)
+
+app.include_router(CBrouter)
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 통신할 URL 적어두면 돼요 나중에 지금은 테스트중 이니깐 모두 허용 상태 입니다. -강창규-
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,12 +21,3 @@ app.add_middleware(
 @app.get("/")
 async def main():
     return "추론서버"
-#
-# 각 기능 별로 컴포넌트 나누시면 React처럼 router 따서 사용하시면 됍니다.
-# from fastapi import APIRouter
-# router = APIRouter()
-# @router.get("api 주소값")
-# async def 함수명(매개변수):
-#   함수 내용  
-#  return 
-# #
