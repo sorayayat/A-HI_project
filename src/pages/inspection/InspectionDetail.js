883d76a449c0f34f793fb9f3422b014f66e77cbc
kang chankyu <gangchanggyu9@gmail.com>
2023-12-22 16:31:06 +0900

    12-22 이력서 자체를 가져오는 페이지 구현 중

---
+++ src/pages/inspection/InspectionDetail.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { callInspectionResumeAPI, callResumeDetailAPI } from "../../apis/inspectionAPICalls";
+
+function InspectionDetail()
+{
+    const navigate = useNavigate();
+    const dispach = useDispatch();
+    const params = useParams();
+
+
+    useEffect(() =>{
+        dispach(callResumeDetailAPI(params.resumeCode);
+    },[])
+
+    return(
+        <div>
+            <h1>Hellow fk World</h1>
+        </div>
+    )
+}
+
+export default InspectionDetail;
