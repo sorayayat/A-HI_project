94e62ca57c6bbcc6c96f0bbd2335cfc89b9e297f
GI_WON <rldnjsqhdl@naver.com>
2023-12-22 11:36:23 +0900

    giwonCOmmit

--- src/main/java/com/jsg/ahispringboot/config/JpaConfig.java
+++ src/main/java/com/jsg/ahispringboot/config/JpaConfig.java
@@ -1,10 +1,7 @@
 package com.jsg.ahispringboot.config;
 
 
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
