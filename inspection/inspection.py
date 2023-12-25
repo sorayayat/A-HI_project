faa76ef6cdb0bbc0070afbb1cddef3c144ed5a05
gangc <gangchanggyu9@gmail.com>
2023-12-25 17:38:57 +0900

    23-12-25 메인서버랑 통신 연결 확인 내일 gpt 연동해서 실험예정

--- inspection/inspection.py
+++ inspection/inspection.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter , Request
+from fastapi import APIRouter , Request , UploadFile , File
+from PyPDF2 import PdfReader
 from typing import List
 from pydantic import BaseModel
 from inspection import inspectionPrompt
+import io
 
 ITrouter = APIRouter(prefix="/inspection")
 
@@ -17,3 +19,18 @@ async def ask(ask : Ask):
     result = inspectionPrompt.create_prediction_prompt(ask)
     print(f"result : {result}")
     return {"status" : 200 , "result" : result}
+
+@ITrouter.post("/ReadResume")
+async def readResume(file : UploadFile = File(...)):
+    contents = await file.read()
+    buffer = io.BytesIO(contents)
+    pdf_reader = PdfReader(buffer)
+
+    pageNumber = 0
+    page = pdf_reader.pages[pageNumber]
+    text = page.extract_text()
+
+    print(f'{text}')
+    print(type(text))
+    # 내일 가서 gpt 한테 해당 내용을 키 와 값으로 구별해줘 을 붙이면 key&value로 구별해주니 키&value로 분류 하고 다시 view 단으로 쏴주기
+    return {"message" : "gogo"}
