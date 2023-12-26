9c538f0863848123898cb391b6595ebb0020db28
giwon0213 <2giwon0213@gmail.com>
2023-12-26 18:29:46 +0900

    vectorDb 실패..

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from fastapi import FastAPI
 from interview.interview import Interview_router
 from fastapi.middleware.cors import CORSMiddleware
 # from inspection.inspection import ITrouter
-from company.posting import COrouter
+from company.posting import POrouter
 from chatbot.chatbot import CBrouter
 from resume.resume import resume_router
+from recommendation.recommendation import RErouter
 
 app = FastAPI()
 
 app.include_router(Interview_router)
 # app.include_router(ITrouter)
-app.include_router(COrouter)
+app.include_router(POrouter)
 app.include_router(CBrouter)
 app.include_router(resume_router)
+app.include_router(RErouter)
 
 
 app.add_middleware(
