f74c540909020ebf010876347a4e26e9a5f41694
giwon0213 <2giwon0213@gmail.com>
2024-01-15 15:50:00 +0900

    giwonCommit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, Path, HTTPException
 from interview.interview import Interview_router
 from fastapi.middleware.cors import CORSMiddleware
-from inspection.inspection import ITrouter
+
 from company.posting import POrouter
 from chatbot.chatbot import CBrouter
 # from resume.resume import resume_router
@@ -15,9 +15,9 @@ app = FastAPI()
 
 
 app.include_router(CBrouter)
-app.include_router(Interview_router)
+
 app.include_router(userInterViewrouter)
-app.include_router(ITrouter)
+
 app.include_router(POrouter)
 app.include_router(RErouter)
 app.include_router(LIrouter)
